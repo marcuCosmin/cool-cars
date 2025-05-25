9d9d094003b0e3d2f42388180e07b7e0724c45fa
Cosmin Marcu <marcucosmin3@yahoo.com>
2025-05-25 22:07:19 +0300

    Complete the about page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,8 +1,19 @@
+import Image from "next/image"
+
 export default function About() {
   return (
-    <main className="h-[calc(100vh-80px)] bg-[url('/about-background.jpg')] bg-top bg-size-[auto_100%] bg-no-repeat">
-      <section className="flex flex-col items-center gap-10 h-full justify-center text-white">
-        <h1 className="font-bold text-center">About Cool Cars South Coast</h1>
+    <main className="flex bg-cover h-[calc(100vh-80px)] bg-[url('/about-background.jpg')] lg:bg-left lg:bg-size-[auto_100%] bg-no-repeat bg-overlay">
+      <Image
+        className="h-full w-auto hidden lg:block"
+        src="/about-background.jpg"
+        alt="Text"
+        width={1024}
+        height={1536}
+      />
+      <section className="flex flex-col items-center gap-10 h-full justify-center lg:justify-start p-5 lg:p-10 lg:text-lg text-white lg:max-w-4xl lg:mx-auto">
+        <h1 className="font-bold text-center lg:text-3xl">
+          About Cool Cars South Coast
+        </h1>
         <p className="text-center">
           Cool Cars South Coast is a family-run business dedicated to providing
           top-notch car body repair and customisation services. Our team of
